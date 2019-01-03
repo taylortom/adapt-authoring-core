ff8e0f89fc0899016955f0b6ee6275aab1b35cf1
Thomas Taylor <thomas.taylor@kineo.com>
2019-01-03 14:34:32 +0000

    Add utils

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const App = require('./lib/app');
 const DataStore = require('./lib/datastore');
 const Module = require('./lib/module');
+const Utils = require('./lib/utils');
+
 
 module.exports = {
   App,
   DataStore,
-  Module
+  Module,
+  Utils
 };
