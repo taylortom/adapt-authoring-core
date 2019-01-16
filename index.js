a49e56ce198d910a908cf207d4fa6e3c956e073a
Thomas Taylor <thomas.taylor@kineo.com>
2019-01-16 13:39:02 +0000

    Add new files to exports

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const App = require('./lib/app');
 const DataStore = require('./lib/datastore');
+const DataStoreQuery = require('./lib/datastorequery');
+const Events = require('./lib/events');
+const Hookable = require('./lib/loadable');
+const Loadable = require('./lib/hookable');
 const Module = require('./lib/module');
+const Responder = require('./lib/responder');
 const Utils = require('./lib/utils');
-
 
 module.exports = {
   App,
   DataStore,
+  DataStoreQuery,
+  Events,
+  Hookable,
+  Loadable,
   Module,
+  Responder,
   Utils
 };
