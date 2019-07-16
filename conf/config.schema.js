911934dc00ac827633f0c30b4cbfe75c4a47ac84
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-16 21:02:14 +0100

    Add config option

--- conf/config.schema.js
+++ conf/config.schema.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
   definition: {
+    root_dir: {
+      type: 'String',
+      default: process.cwd(),
+      description: 'Path to the root folder'
+    },
     temp_dir: {
       type: 'String',
       default: path.join(process.cwd(), 'temp')
